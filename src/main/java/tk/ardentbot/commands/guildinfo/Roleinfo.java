2d4e59d9c6bcdebc6b44b61b1c5f32150d8bccdf
adugar <armaandugar1@gmail.com>
2017-04-29 15:32:34 -0400

    combo's work

--- src/main/java/tk/ardentbot/commands/guildinfo/Roleinfo.java
+++ src/main/java/tk/ardentbot/commands/guildinfo/Roleinfo.java
@@ -3,13 +3,9 @@ package tk.ardentbot.commands.guildinfo;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.*;
 import tk.ardentbot.core.executor.Command;
-import tk.ardentbot.core.translate.Language;
-import tk.ardentbot.core.translate.Translation;
-import tk.ardentbot.core.translate.TranslationResponse;
 import tk.ardentbot.utils.discord.MessageUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Roleinfo extends Command {
@@ -18,77 +14,68 @@ public class Roleinfo extends Command {
     }
 
     @Override
-    public void noArgs(Guild guild, MessageChannel channel, User user, Message message, String[] args, Language language) throws Exception {
+    public void noArgs(Guild guild, MessageChannel channel, User user, Message message, String[] args) throws Exception {
         List<Role> mentionedRoles = message.getMentionedRoles();
         if (args.length == 1 && mentionedRoles.size() == 0) {
-            sendRetrievedTranslation(channel, "roleinfo", language, "mentionuserortyperolename", user);
+            sendTranslatedMessage("Mention one or more roles or type a role's name to see its info!", channel, user);
             return;
         }
         if (mentionedRoles.size() == 1) {
-            sendEmbed(getRoleInformation(mentionedRoles.get(0), guild, language, user), channel, user);
-        }
-        else {
+            sendEmbed(getRoleInformation(mentionedRoles.get(0), guild, user), channel, user);
+        } else {
             for (Role r : mentionedRoles) {
-                sendEmbed(getRoleInformation(r, guild, language, user), channel, user);
+                sendEmbed(getRoleInformation(r, guild, user), channel, user);
             }
         }
         String roleString = replaceCommandIdAndPrefix(message.getRawContent());
         List<Role> retrievedRoles = guild.getRolesByName(roleString, true);
         if (retrievedRoles.size() == 0) {
-            sendRetrievedTranslation(channel, "roleinfo", language, "mentionuserortyperolename", user);
-        }
+            sendTranslatedMessage("Mention one or more roles or type a role's name to see its info!", channel, user);
+        } else if (retrievedRoles.size() > 1) {
+            sendTranslatedMessage("Multiple roles were found... Please select the correct one from the below list", channel, user);
+            sendEmbed(chooseFromList("Choose a role", guild, user,
 
-        else if (retrievedRoles.size() > 1) {
-            sendRetrievedTranslation(channel, "roleinfo", language, "multiplerolesfound", user);
-            sendEmbed(chooseFromList(getTranslation("roleinfo", language, "choosefromlist").getTranslation(), guild, language, user,
                     this, asStringArray(retrievedRoles)), channel, user);
-            interactiveOperation(language, channel, message, selectionMessage -> {
+            interactiveOperation(channel, message, selectionMessage -> {
                 try {
                     Role role = retrievedRoles.get(Integer.parseInt(selectionMessage.getContent()) - 1);
-                    sendEmbed(getRoleInformation(role, guild, language, user), channel, user);
-                }
-                catch (Exception e) {
-                    sendRetrievedTranslation(channel, "tags", language, "invalidarguments", user);
+                    sendEmbed(getRoleInformation(role, guild, user), channel, user);
+                } catch (Exception e) {
+                    sendTranslatedMessage("Invalid Argument", channel, user);
                 }
             });
-        }
-        else if (retrievedRoles.size() == 1) sendEmbed(getRoleInformation(retrievedRoles.get(0), guild, language, user), channel, user);
+        } else if (retrievedRoles.size() == 1)
+            sendEmbed(getRoleInformation(retrievedRoles.get(0), guild, user), channel, user);
     }
 
     @Override
     public void setupSubcommands() throws Exception {
     }
 
-    private EmbedBuilder getRoleInformation(Role role, Guild guild, Language language, User user) throws Exception {
-        EmbedBuilder builder = MessageUtils.getDefaultEmbed(guild, user, this);
-        HashMap<Integer, TranslationResponse> translations = getTranslations(language, new Translation("roleinfo", "title"),
-                new Translation("roleinfo", "rolename"), new Translation("roleinfo", "memberswith"),
-                new Translation("roleinfo", "creationtime"), new Translation("roleinfo", "color"),
-                new Translation("roleinfo", "permissions"));
-        String title = translations.get(0).getTranslation();
+    private EmbedBuilder getRoleInformation(Role role, Guild guild, User user) throws Exception {
+        EmbedBuilder builder = MessageUtils.getDefaultEmbed(user);
+        String title = "Role Info | Server Specific";
         builder.setAuthor(title, guild.getIconUrl(), guild.getIconUrl());
-        builder.addField(translations.get(1).getTranslation(), role.getName(), true);
-        builder.addField(translations.get(2).getTranslation(), String.valueOf(guild.getMembers().stream().filter(member -> {
+        builder.addField("Role name", role.getName(), true);
+        builder.addField("# with role", String.valueOf(guild.getMembers().stream().filter(member -> {
             boolean found = false;
             for (Role r : member.getRoles()) {
                 if (r.getId().equals(role.getId())) found = true;
             }
             return found;
         }).count()), true);
-        builder.addField(translations.get(3).getTranslation(), role.getCreationTime().toLocalDate().toString(), true);
+        builder.addField("Creation time", role.getCreationTime().toLocalDate().toString(), true);
         try {
-            builder.addField(translations.get(4).getTranslation(), "#" + Integer.toHexString(role.getColor().getRGB()).substring(2)
+            builder.addField("Hex color", "#" + Integer.toHexString(role.getColor().getRGB()).substring(2)
                     .toUpperCase(), true);
-        }
-        catch (NullPointerException npe) {
-            builder.addField(translations.get(4).getTranslation(), "#ffffff", true);
+        } catch (NullPointerException npe) {
+            builder.addField("Hex color", "#ffffff", true);
         }
         ArrayList<String> permissions = new ArrayList<>();
         role.getPermissions().forEach(permission -> permissions.add(permission.getName()));
         try {
-            builder.addField(translations.get(5).getTranslation(), MessageUtils.listWithCommas(permissions), true);
-        }
-        catch (Exception ignored) {
+            builder.addField("Permissions", MessageUtils.listWithCommas(permissions), true);
+        } catch (Exception ignored) {
         }
         return builder;
     }
